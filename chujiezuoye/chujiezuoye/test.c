78fe80271d8bab4b6225969ee0ecd5a6f2e3da8c
shaoyaojiangli <1909914367@qq.com>
2023-07-25 23:33:00 +0800

    初阶作业——未完

---
+++ chujiezuoye/chujiezuoye/test.c
@@ -0,0 +1,211 @@
+#include <stdio.h>
+#include <assert.h>
+
+//int my_strlen(const char* str)
+//{
+//	int count = 0;
+//	assert(str);
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+
+//int main()
+//{
+//	char arr[] = "hello bit";
+//	int len = my_strlen(arr);
+//	printf("%d\n", len);
+//	return 0;
+//}
+
+
+//-----------------------给出三条边，判断是否可以构成三角形，如果可以，请判断是那种三角形
+
+
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	while (scanf_s("%d %d %d", &a, &b, &c) == 3)
+//	{
+//		//判断
+//		if (a + b > c && a + c > b && b + c > a)
+//		{
+//			if (a == b && b == c)
+//			{
+//				printf("equilateral triangle!\n");
+//			}
+//			else if (a == b || a == c || b == c)
+//			{
+//				printf("isosceis triangle!\n");
+//
+//			}
+//			else
+//			{
+//				printf("ordinary triangle!\n");
+//			}
+//		}
+//		else
+//		{
+//			printf("not a triangle!\n");
+//		}
+//	}
+//	return 0;
+//}
+
+//数据在内存中存放的时候有一个顺序的问题      大小端字节序
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5 };
+//	short* p = (short*)arr;
+//	int i = 0;
+//	for (i = 0; i < 4; i++)
+//	{
+//		*(p + i) = 0;
+//	}
+//
+//	for (i = 0; i < 5; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	unsigned long pulArray[] = { 6,7,8,9,10 };
+//	unsigned long* pulPtr;
+//
+//	pulPtr = pulArray;
+//	*(pulPtr + 3) += 3;
+//
+//	printf(" % d, % d\n", *pulPtr, *(pulPtr + 3));
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0x11223344;
+//	char* pc = (char*)&a;
+//	*pc = 0;
+//	printf("%x\n", a);
+//	return 0;
+//}
+
+
+//------------------------------------写一个函数打印arr数组的内容，不使用数组的下标，使用指针
+
+//void print(int* p, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", *(p + i));
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(arr, sz);
+//	return 0;
+//}
+
+
+//---------------------------------写一个函数可以逆序字符串
+//#include <string.h>
+//int main()
+//{
+//	char arr[10001] = { 0 };
+//	//由于scanf不能读取空格，所以采用gets
+//	gets(arr);
+//	//逆序
+//	int left = 0;
+//	int right = strlen(arr) - 1;
+//
+//	while (left < right)
+//	{
+//		char tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//
+//		left++;
+//		right--;
+//	}
+//
+//	printf("%s\n", arr);
+//	return 0;
+//}
+
+
+
+
+///-----------------------------------
+//求Sn= a + aa + aaa + aaaa + aaaaa的前五项之和，其中a是一个数字
+
+//
+//int main()
+//{
+//	int a = 0;
+//	int n = 0;
+//	scanf_s("%d %d", &a, &n);
+//	int i = 0;
+//	int sum = 0;
+//	int k = 0;
+//	for (i = 0; i < n; i++)
+//		{
+//		k = k * 10 + a;
+//		sum += k;
+//		}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+
+
+//---------------------------------------------打印水仙花数，求0~10000的水仙花数
+//  水仙花数是指一个n位数，其各位数字的n次方之和刚好等于该数本身。
+#include <math.h>
+int main()
+{
+	int i = 0;
+	for (i + 0; i <= 100000; i++)
+	{
+		
+		//判断i是否为水仙花数
+		//1.计算i是几位数
+		
+		int n = 1;
+		int tmp = i;
+		int sum = 0;
+		while (tmp / 10)
+		{
+			n++;
+			tmp /= 10;
+		}
+
+		//2.得到i的每一位，计算他的n次方之和
+		tmp = i;
+		while (tmp)
+		{
+			sum += pow(tmp % 10, n);
+			tmp /= 10;
+		}
+		if (sum == i)
+		{
+			printf("%d ",i);
+		}
+
+	}
+	return 0;
+}
