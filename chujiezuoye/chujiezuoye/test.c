251353026131dc52ad1536268b7a26ef2665df8e
shaoyaojiangli <1909914367@qq.com>
2023-07-26 17:34:10 +0800

    初阶作业已完成

--- chujiezuoye/chujiezuoye/test.c
+++ chujiezuoye/chujiezuoye/test.c
@@ -173,39 +173,235 @@
 
 
 
-//---------------------------------------------打印水仙花数，求0~10000的水仙花数
-//  水仙花数是指一个n位数，其各位数字的n次方之和刚好等于该数本身。
-#include <math.h>
+////---------------------------------------------打印水仙花数，求0~10000的水仙花数
+////  水仙花数是指一个n位数，其各位数字的n次方之和刚好等于该数本身。
+//#include <math.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i + 0; i <= 100000; i++)
+//	{
+//		
+//		//判断i是否为水仙花数
+//		//1.计算i是几位数
+//		
+//		int n = 1;
+//		int tmp = i;
+//		int sum = 0;
+//		while (tmp / 10)
+//		{
+//			n++;
+//			tmp /= 10;
+//		}
+//
+//		//2.得到i的每一位，计算他的n次方之和
+//		tmp = i;
+//		while (tmp)
+//		{
+//			sum += pow(tmp % 10, n);
+//			tmp /= 10;
+//		}
+//		if (sum == i)
+//		{
+//			printf("%d ",i);
+//		}
+//
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int line = 0;
+//	scanf_s("%d", &line);
+//	int i = 0;
+//	for (i = 0; i < line; i++)
+//	{
+//		//空格
+//		int j = 0;
+//		for (j = 0; j < line - 1 - i; j++)
+//		{
+//			printf(" ");
+//		}
+//		//*
+//		for (j = 0; j < 2 * i + 1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//
+//
+//
+//	for (i = 0; i < line - 1; i++)
+//	{
+//		//空格
+//		int j = 0;
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf(" ");
+//		}
+//		//*
+//		for (j = 0; j < 2*(line-1-i)-1; j++)
+//		{
+//			printf("*");
+//
+//		}
+//		printf("\n");
+//
+//	}
+//	return 0;
+//}
+
+
+//喝汽水，1瓶汽水一元，2个空瓶可以换一瓶汽水，20元，可以换多少汽水
+
+//int main()
+//{
+//	int money = 0;
+//	scanf_s("%d", &money);
+//	int total = money;
+//	int empty = money;
+//
+//	//置换
+//	while (empty >= 2)
+//	{
+//		total += empty / 2;
+//		empty = empty / 2 + empty % 2;
+//	}
+//	printf("%d\n", total);
+//
+//	return 0;
+//}
+
+//
+//int cnt = 0;
+//int fib(int n)
+//{
+//	cnt++;
+//	if (n == 0)
+//	{
+//		return 1;
+//	}
+//	else if (n == 1)
+//	{
+//		return 2;
+//	}
+//	else
+//	{
+//		return fib(n - 1) + fib(n - 2);
+//	}
+//}
+//
+//void main()
+//{
+//	fib(8);
+//		printf("%d", cnt);
+//}
+////67
+
+
+
+//int x = 1;
+//do{
+//	printf("%2d\n", x++);
+//} while (x--);
+////陷入死循环
+
+
+
+
+//#include<stdlib.h>
+//int a = 1;
+//void test()
+//{
+//	int a = 2;
+//	a += 1;
+//}
+//
+//int main()
+//{
+//	test();
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//设计一个算法，求输入A和B的最小公倍数
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf_s("%d %d", &a, &b);
+//	//计算最小公倍数
+//	int m = (a > b ? a : b);
+//	while (1)
+//	{
+//		if (m % a == 0 && m % b == 0)
+//		{
+//			break;
+//		}
+//		m++;
+//	}
+//	printf("%d\n", m);
+//
+//	return 0;
+//}
+
+
+// ----------------------------- 倒置单词
+//1.把整个字符串逆序
+//2. 把每个单词逆序
+#include <assert.h>
+#include <string.h>
+
+void reverse(char* left, char* right)
+{
+	assert(left);
+	assert(right);
+
+	while (left < right)
+	{
+		char tmp = *left;
+		*left = *right;
+		*right = tmp;
+		left++;
+		right--;
+	}
+}
+
+
 int main()
 {
-	int i = 0;
-	for (i + 0; i <= 100000; i++)
+	//输入
+	char arr[10] = { 0 };
+	gets(arr);
+	//逆置
+	int len = strlen(arr);
+	//逆序整个字符串
+	reverse(arr, arr + len - 1);
+	//逆序每个单词
+	char* start = arr;
+	while (*start)
 	{
-		
-		//判断i是否为水仙花数
-		//1.计算i是几位数
-		
-		int n = 1;
-		int tmp = i;
-		int sum = 0;
-		while (tmp / 10)
+		char* end = start;
+		while (*end != ' ' && *end != '\0')
 		{
-			n++;
-			tmp /= 10;
+			end++;
 		}
-
-		//2.得到i的每一位，计算他的n次方之和
-		tmp = i;
-		while (tmp)
+		reverse(start, end - 1);
+		if (*end != '\0')
 		{
-			sum += pow(tmp % 10, n);
-			tmp /= 10;
+			end++;
 		}
-		if (sum == i)
-		{
-			printf("%d ",i);
-		}
-
+		start = end;
 	}
+
+	//输出
+
+	printf("%s\n", arr);
+
 	return 0;
 }
